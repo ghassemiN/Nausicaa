0e63081f39c9107716390ccd28941ed76fa80d98
nGhassemi <nasy.ghassemi@gmail.com>
2016-09-05 02:36:08 +0430

    Modified search-box and little change in mystyle

--- header.php
+++ header.php
@@ -52,6 +52,26 @@
 					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
 				<?php endif; ?>
 			</div>
+			<div class="head-search hide-for-small-only">
+				<?php do_action( 'foundationpress_before_searchform' ); ?>
+				<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+					<?php do_action( 'foundationpress_searchform_top' ); ?>
+					<div class="input-group">
+						<input type="text" class="input-group-field" value="" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'foundationpress' ); ?>">
+						<?php do_action( 'foundationpress_searchform_before_search_button' ); ?>
+						<div class="input-group-button">
+							<button  type="submit" id="searchsubmit" class="button search-button">
+								<i class="fa fa-search fa-4" aria-hidden="true"></i>
+							</button>
+							
+						</div>
+
+					</div>
+					<?php do_action( 'foundationpress_searchform_after_search_button' ); ?>
+				</form>
+				<?php do_action( 'foundationpress_after_searchform' ); ?>
+			</div>
+
 		</nav>
 
 	</header>
